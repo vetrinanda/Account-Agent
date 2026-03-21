7da563206048f9f041c6db074ab918d3cbcd2473
Nanda Kumar <148224808+vetrinanda@users.noreply.github.com>
2026-03-21 22:33:52 +0530

    Update agents.py

--- backend/app/agents.py
+++ backend/app/agents.py
@@ -1,7 +1,9 @@
 from langchain_classic.agents import create_agent
 from.llm import llm
-
+from langchain_corr.messages important ChatPromptTemplate
 agent=create_agent(
     model=llm,
-    system_prompt="You are a helpful assistant that can answer questions and perform tasks.",
-    tools=[])
+    system_prompt=prompt,
+    tools=[]) 
+
+prompt =ChatPromptTemplate.from_messages ('system', "You are a helpful assistant that can answer questions and perform tasks.") 
