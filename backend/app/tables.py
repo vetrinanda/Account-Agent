201b62e75c34324d9f092cc00b967d8dc8bc6a58
Nanda Kumar <nk9219382@gmail.com>
2026-03-22 22:04:58 +0530

    fix:Updated the schemas once again

--- backend/app/tables.py
+++ backend/app/tables.py
@@ -6,5 +6,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
+    balance=Column(Integer,primary_key=True,index=True)
+    nonlocalame = Column(String, index=True)
+    email = Column(String, index=True)
