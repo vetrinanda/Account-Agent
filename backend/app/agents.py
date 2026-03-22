a8738c774410d68ce46dbeaccc1ad061d8c26bfe
Nanda Kumar <nk9219382@gmail.com>
2026-03-22 21:57:17 +0530

    fix: Added prompt definiton before using it in the code

--- backend/app/agents.py
+++ backend/app/agents.py
@@ -1,9 +1,13 @@
 from langchain_classic.agents import create_agent
 from.llm import llm
 from langchain_core.messages import ChatPromptTemplate
+
+
+prompt =ChatPromptTemplate.from_messages ('system', "You are a helpful assistant that can answer questions and perform tasks.") 
+
+
 agent=create_agent(
     model=llm,
     system_prompt=prompt,
     tools=[]) 
 
-prompt =ChatPromptTemplate.from_messages ('system', "You are a helpful assistant that can answer questions and perform tasks.") 
